2e39c88c0f0913e890a40c8da84d8319a636e1b1
kevinfrias051@gmail.com <kevinfrias051@gmail.com>
2024-05-16 14:24:34 -0300

    acomodaciones :D

--- src/models/usuario.dto.ts
+++ src/models/usuario.dto.ts
@@ -1,17 +1,28 @@
-import { IsInt, IsOptional, IsString } from 'class-validator';
+import { IsInt, IsOptional, IsString, isString } from 'class-validator';
 
 class UsuarioDto {
   userId?: number;
   @IsOptional()
   email: string;
+
   @IsOptional()
   password: string;
   
+  @IsOptional()
   telefono: number;
+
+  @IsOptional()
   provincia: string;
+
+  @IsOptional()
   ciudad: string;
+
+  @IsOptional()
   codigoPostal: string;
+  
+  @IsOptional()
   direccion: string;
+
   activo?: boolean;
   admin?:boolean;
 }
